b9fe86b840543f8eba7db426018b01e9dae256ab
samiyonas <samuel.x.yonas@gmail.com>
2024-03-05 08:47:18 -0800

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Shell sort algorithm
+ * @array: unsorted data
+ * @size: large of array
+ * Return: Nothing
+ */
+void shell_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t j, gap, n;
+
+	gap = 1;
+	if (array == NULL || size < 2)
+		return;
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+	while (gap > 0)
+	{
+		for (j = gap; j < size; j += 1)
+		{
+			tmp = array[j];
+			for (n = j; n >= gap && tmp < array[n - gap]; n -= gap)
+				array[n] = array[n - gap];
+			array[n] = tmp;
+		}
+		gap /= 3;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -23,5 +23,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void swap(listint_t **head, listint_t **n1, listint_t *n2);
 void selection_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
 
 #endif
